11ec50033a12d768fceb2355d47c19955d67c22b
Jenni <valijenni@hotmail.com>
2025-03-20 13:45:59 +0200

    Sign up UI

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,9 +1,8 @@
 import React from 'react';
-import {View, Text, Button, StyleSheet,Image} from "react-native";
+import {View, Text, ScrollView, StyleSheet,Image,KeyboardAvoidingView,Platform} from "react-native";
 import { TextInput } from 'react-native-paper'
 import Background from '../components/Background';
 import { Pressable } from 'react-native';
-import { TouchableOpacity } from 'react-native-gesture-handler';
 const Component = require('../../assets/Component 3.png');
 
 
@@ -11,52 +10,67 @@ export default function LoginScreen({ navigation }) {
     return (
         
         <Background>
-        <View style={styles.container}>
-            <Image source={Component} style={{width: 305, height: 159,resizeMode: 'contain'}} />
-            <Text style={{fontSize: 48,fontWeight:'bold'}}>Hello!</Text>
-            <Text>Sign in your account</Text>
-            <View style={{flexDirection: 'colum',marginTop: 20,marginBottom: 10}}>
-                <View style={{flexDirection: 'row',alignItems: 'center',width:'90%', marginTop: 10,
-                            marginBottom: 10,}} >
-                   
-                    <TextInput 
-                        style={{width: '100%',
-                        marginTop: 10}}
-                        label="Username"
-                        mode= "outlined" 
-                        left={<TextInput.Icon icon="account" />}
-                        theme={{ roundness: 15 }} 
-                        />
-                </View>
-                <View>
-                    <TextInput style={{width: '100%',marginTop: 20}} 
-                    label="Password"
-                    mode="outlined"
-                    secureTextEntry
-                    left={<TextInput.Icon icon="lock"/>} 
-                    theme={{roundness: 15}}/>
-                </View>
-            </View>
+            <KeyboardAvoidingView
+                behavior={Platform.OS === "ios" ? "padding" : "height"}
+                style={styles.container}
+            >
+            
+                
+                <ScrollView contentContainerStyle={{flexGrow: 1,justifyContent: 'center',
+                            alignItems: 'center',
+                            paddingTop: 20,
+                            paddingBottom: 20,}}>
+                    <View style={{marginTop: 40}}>
+                        <Image source={Component} style={{width: 305, height: 159,resizeMode: 'contain'}} />
+                    </View>
+                    <Text style={{fontSize: 50,fontWeight:'bold',}}>Hello!</Text>
+                    <Text>Sign in your account</Text>
+                    <View style={{flexDirection: 'colum',marginTop: 20,marginBottom: 10}}>
+                        <View style={{flexDirection: 'row',alignItems: 'center',width:'90%', marginTop: 10,
+                                    marginBottom: 10,}} >
+                        
+                            <TextInput 
+                                style={{width: '100%',
+                                marginTop: 10}}
+                                placeholder="Username"
+                                mode= "outlined" 
+                                left={<TextInput.Icon icon="account" />}
+                                theme={{ roundness: 15 }} 
+                                />
+                        </View>
+                        <View style={{flexDirection: 'row',alignItems: 'center',width:'90%', marginTop: 10,
+                                    marginBottom: 10,}}>
+                            <TextInput style={{width: '100%'}} 
+                            placeholder="Password"
+                            mode="outlined"
+                            secureTextEntry
+                            left={<TextInput.Icon icon="lock"/>} 
+                            theme={{roundness: 15}}/>
+                        </View>
+                    </View>
 
-            {/* Sign Up Link */}
-            <Pressable onPress={() => navigation.navigate('Signup')}>
-                    <Text>Don't have an account?
-                        <Text style={{color:'purple',textDecorationLine:'underline'}}> Sign up</Text>
-                    </Text>
-            </Pressable>
+                    {/* Sign Up Link */}
+                    <Pressable onPress={() => navigation.navigate('Signup')}>
+                            <Text style={{fontSize:18}}>Don't have an account?
+                                <Text style={{color:'purple',textDecorationLine:'underline'}}> Sign up</Text>
+                            </Text>
+                    </Pressable>
 
-           
-        </View>
+                    {/* Login Button */}
+                    <View style={{alignItems:'center',marginTop: 20,marginBottom: 40}}>
+                        <Pressable 
+                            style={{backgroundColor: '#9859FC',alignItems:'center',width:200,borderRadius:30,paddingVertical: 12,
+                            paddingHorizontal: 32,marginTop: 20}} 
+                            onPress={() => navigation.navigate('Main')}>
+                                <Text style={{color:'white',fontSize:18}}>Submit</Text>
+                        </Pressable>
+                    </View>
 
-         {/* Login Button */}
-         <View style={{alignItems:'center',marginTop: 20,marginBottom: 40}}>
-            <Pressable 
-                style={{backgroundColor: '#9859FC',alignItems:'center',width:200,borderRadius:30,paddingVertical: 12,
-                paddingHorizontal: 32,}} 
-                onPress={() => navigation.navigate('Home')}>
-                    <Text style={{color:'white',fontSize:18}}>Submit</Text>
-            </Pressable>
-        </View>
+                    </ScrollView>
+                
+            
+            
+            </KeyboardAvoidingView>
         </Background>
     );
 }
@@ -67,5 +81,6 @@ const styles = StyleSheet.create({
         paddingTop: 20,
         justifyContent: 'center',
         alignItems: 'center',
+        
     },
 });
--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,20 +1,78 @@
 import React from 'react';
-import {View, Text, Button, StyleSheet} from "react-native";
+import {View, Text, Button, StyleSheet,Image,Pressable,KeyboardAvoidingView,Platform,ScrollView} from "react-native";
+import { TextInput } from 'react-native-paper'
 import Background from '../components/Background';
+const Component = require('../../assets/Component 3.png');
+
 
 export default function SignupScreen({ navigation }) {
     return (
         <Background>
-        <View style={styles.container}>
-            <Text>Signup Screen</Text>
-            <Button title="Go to Profile" onPress={() => navigation.navigate('Profile')} />
-            <Button title="Go to Forum" onPress={() => navigation.navigate('Forum')} />
-            <Button title="Go to Info" onPress={() => navigation.navigate('Info')} />
-            <Button title="Go to Reviews" onPress={() => navigation.navigate('Reviews')} />
-            <Button title="Go to Search" onPress={() => navigation.navigate('Search')} />
-            <Button title="Go to Login" onPress={() => navigation.navigate('Login')} />
-            <Button title="Go to Signup" onPress={() => navigation.navigate('Signup')} />
-        </View>
+            <KeyboardAvoidingView
+                            behavior={Platform.OS === "ios" ? "padding" : "height"}
+                            style={styles.container}
+                        >
+                
+                    <ScrollView contentContainerStyle={{flexGrow: 1,justifyContent: 'center',
+                                                alignItems: 'center',
+                                                paddingTop: 20,
+                                                paddingBottom: 20,}}>
+                        <View style={{marginTop: 40}}>
+                            <Image source={Component} style={{width: 305, height: 159,resizeMode: 'contain'}} />
+                        </View>
+                        <Text style={{fontSize: 36,fontWeight:'600',}}>Create an account</Text>
+                        <View style={{flexDirection: 'colum',marginTop: 20,marginBottom: 10}}>
+                            <View style={{flexDirection: 'row',alignItems: 'center',width:'90%', marginTop: 10,
+                                        marginBottom: 10,}}>
+                                <TextInput style={{width: '100%',marginTop: 10}} 
+                                    placeholder="Email"
+                                    mode="outlined"
+                                    left={<TextInput.Icon icon="email"/>} 
+                                    theme={{roundness: 15}}/>
+                            </View>
+                            <View style={{flexDirection: 'row',alignItems: 'center',width:'90%',
+                                        marginBottom: 10,}} >
+                            
+                                <TextInput 
+                                    style={{width: '100%',
+                                    marginTop: 10}}
+                                    placeholder="Username"
+                                    mode= "outlined" 
+                                    left={<TextInput.Icon icon="account" />}
+                                    theme={{ roundness: 15 }} 
+                                    />
+                            </View>
+                            <View>
+                                <TextInput style={{width: '100%',marginTop: 10}} 
+                                placeholder="Password"
+                                mode="outlined"
+                                secureTextEntry
+                                left={<TextInput.Icon icon="lock"/>} 
+                                theme={{roundness: 15}}/>
+                            </View>
+                            
+                        </View>
+
+                        {/* Sign In Link */}
+                                    <Pressable onPress={() => navigation.navigate('Login')}>
+                                            <Text style={{fontSize:18}}>Already have an account?
+                                                <Text style={{color:'purple',textDecorationLine:'underline'}}> Log in</Text>
+                                            </Text>
+                                    </Pressable>
+
+                        {/* Sign up Button */}
+                                <View style={{alignItems:'center',marginTop: 20,marginBottom: 40}}>
+                                    <Pressable 
+                                        style={{backgroundColor: '#9859FC',alignItems:'center',width:200,borderRadius:30,paddingVertical: 12,
+                                        paddingHorizontal: 32,marginTop: 20}} 
+                                        onPress={() => navigation.navigate('Login')}>
+                                            <Text style={{color:'white',fontSize:18}}>Submit</Text>
+                                    </Pressable>
+                        </View>
+                    </ScrollView>
+                
+                
+            </KeyboardAvoidingView>
         </Background>
     );
 }
